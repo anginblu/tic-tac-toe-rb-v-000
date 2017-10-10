4ff1e101b082a979f30eb3cab74f482791f4ddd6
Jacqueline Lee <anginblu@gmail.com>
2017-10-10 03:08:32 +0000

    automatically backed up by learn

---
+++ tic_tac_toe_OO.rb
@@ -0,0 +1,62 @@
+class TicTacToe
+
+  def initialize(board=nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def turn_count
+  @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def position_taken?
+    @board[@index] != " " && @board[@index] != ""
+  end
+
+  def valid_move?
+    @index.between?(0,8) && !position_taken?
+  end
+
+  def move
+    @board[@index] = current_player
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    @index = input_to_index(input)
+    if valid_move?
+      move
+      display_board
+    else
+      turn
+    end
+  end
+
+  def play
+    count = 1
+    while count < 10
+      count += 1
+      turn
+    end
+  end
+
+
+end
